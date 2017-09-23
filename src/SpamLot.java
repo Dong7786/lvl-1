75d8c13b6338a0849eb1112646d3d9a7e532a0c7
TheSirLancelot <league@WTS-IM-6.local>
2017-09-23 15:59:16 -0700

    Stuff

---
+++ src/SpamLot.java
@@ -0,0 +1,9 @@
+import javax.swing.JTextField;
+
+public class SpamLot {
+public static void main(String[] args) {
+	JTextField field = new JTextField(50);
+}
+}
+//username = bob573952
+//password = vEk467bef
