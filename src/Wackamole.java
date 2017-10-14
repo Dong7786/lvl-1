46754687f2244343383c5db4f6e703a37a7dad6e
TheSirLancelot <league@WTS-IM-6.attlocal.net>
2017-10-14 15:59:17 -0700

    Set size for wackamole

--- src/Wackamole.java
+++ src/Wackamole.java
@@ -1,4 +1,23 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Wackamole {
-
+	static JFrame frame = new JFrame();
+static JPanel panel = new JPanel();
+public static void main(String[] args) {
+	frame.setSize(500, 500);
+	frame.setVisible(true);
+	frame.add(panel);
+	
+	
+}
+void drawButtons() {
+	JButton buttons[] = new JButton[24];
+	for(int i = 0; i < buttons.length; i ++) {
+		buttons[i] = new JButton();
+		panel.add(buttons[i]);
+		buttons[i].setVisible(true);
+}
+}
 }
