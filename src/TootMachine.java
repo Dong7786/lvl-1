87c7c0d67b1033532525c27317e255475b54bc80
TJHL <league@WTS-IM-6.attlocal.net>
2017-10-28 16:02:12 -0700

    Stuff

---
+++ src/TootMachine.java
@@ -0,0 +1,56 @@
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TootMachine implements ActionListener{
+	JButton b1 = new JButton("Long Fart");
+	JButton b2 = new JButton("Common Fart");
+	JButton b3 = new JButton("Sharp Fart");
+	public static void main(String[] args) {
+		TootMachine g = new TootMachine();
+		g.Goo();
+
+	}
+
+	public void Goo() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.pack();
+		frame.setSize(400, 100);
+		frame.add(panel);
+		
+		frame.setVisible(true);
+		JButton b1 = new JButton("Long Fart");
+		JButton b2 = new JButton("Common Fart");
+		JButton b3 = new JButton("Sharp Fart");
+		panel.add(b1);
+		panel.add(b2);
+		panel.add(b3);
+		
+b1.setVisible(true);
+b2.setVisible(true);
+b3.setVisible(true);
+	b1.addActionListener(this);
+	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(b1)) {
+			System.out.println("test");
+			playSound("/Users/league/Desktop/lf.webloc");
+		}
+	}
+	private void playSound(String fileName) {
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
+	}
+
+
+}
--- src/Wackamole.java
+++ src/Wackamole.java
@@ -1,15 +1,19 @@
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 public class Wackamole implements ActionListener{
 	static int l;
 	static JButton buttons[] = new JButton[84];
-
+static int whacks;
+static int whack;
+static int miss;
 public static void main(String[] args) {
 	
 Wackamole w = new Wackamole();
@@ -36,16 +40,35 @@ void drawButtons() {
 int number = random.nextInt(buttons.length);
 l = number;
 buttons[number].setText("Mole!");
+
 }//drawButtons
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
+	Random r = new Random();
 	if(e.getSource() == buttons[l]) {
-		System.out.println("c");
+	whack++;
 		speak("Whack!");
 	}else {
-		System.out.println("w");
+	miss++;
+	whacks--;
 		speak("Dork!");
+	}
+	if(e.getSource() == buttons[l]) {
+		drawButtons();
+		//int num = r.nextInt(buttons.length);
+		//l = num;
+		//buttons[num].setText("Mole!");
+		//System.out.println("P");
+		whacks++;
+	}
+	
+	if(whacks == 10) {
+		System.out.println("You whacked the mole "+ whack +" times!");
+		System.out.println("You missed the mole "+ miss + " times!");
+
+		System.exit(0);
+				
 	}
 }
 void speak(String words) {
@@ -55,5 +78,6 @@ Runtime.getRuntime().exec("say " + words).waitFor();
 e.printStackTrace();
 }
 }
+
 
 }//class
