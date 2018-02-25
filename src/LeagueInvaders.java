adf554472937b42c8755098c22c7e574653e16f7
Dong Zou <jiwenzou@gmail.com>
2018-02-24 16:01:00 -0800

    LeagueInvaders
    Stopped Pt. 5 #12

---
+++ src/LeagueInvaders.java
@@ -0,0 +1,34 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class LeagueInvaders {
+
+	final int WIDTH = 500;
+	final int HEIGHT = 800;
+	JFrame frame;
+	GamePanel panel;
+
+	public static void main(String[] args) {
+		LeagueInvaders league = new LeagueInvaders();
+	}
+
+	LeagueInvaders() {
+		panel = new GamePanel();
+		frame = new JFrame();
+		setup();
+
+	}
+
+	void setup() {
+		frame.add(panel);
+		frame.getContentPane().setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+		frame.pack();
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.startGame();
+		frame.addKeyListener(panel);
+	}
+}
